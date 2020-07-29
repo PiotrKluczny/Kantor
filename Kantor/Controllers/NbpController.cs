33ed3d9e4c5fbc104fdd0f01eaa12e94575eaa42
Piotr Kluczny <pk.kluczny@gmail.com>
2020-07-29 23:47:34 +0200

    added function related to creation json file

--- Kantor/Controllers/NbpController.cs
+++ Kantor/Controllers/NbpController.cs
@@ -80,13 +80,10 @@ namespace Kantor.Controllers
             int count = values.Count();
             if (count > 1)
             {
-                //Compute the Average
                 double avg = values.Average();
 
-                //Perform the Sum of (value-avg)^2
                 double sum = values.Sum(d => (d - avg) * (d - avg));
 
-                //Put it all together
                 ret = Math.Sqrt(sum / count);
             }
             return ret;
